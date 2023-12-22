8f93340100e9e009e6cc24e2a14c3adbb14a1bcb
José Pablo Muñoz Zúñiga <josepablo.mz04@gmail.com>
2023-12-22 15:54:50 -0600

    Proyecto Final de Programación II

---
+++ SEGUNDOPROYECTODEPROGRAMACIONII/Clases/DBConn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+
+// Segundo Proyecto de Programación II
+// Estudiantes: José Pablo Muñoz Zúñiga 
+//              Karina Marina Marina Unfried Montoya
+//              Dillan Josue Obando Samudio
+// Carrera: Ingeniería Informática
+// Materia: Programación II
+
+namespace SEGUNDOPROYECTODEPROGRAMACIONII.Clases
+{
+
+    // En esta parte es toda la programación de la clase DBConn, para que esta parte del programa funcione correctamente
+    public class DBConn
+    {
+        // En esta parte sirve para extraer todos los datos del sql server management studio al visual studio en C#
+        public static SqlConnection obtenerConexion()
+        {
+            string s = System.Configuration.ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+            SqlConnection conexion = new SqlConnection(s);
+            conexion.Open();
+            return conexion;
+        }
+    }
+}
